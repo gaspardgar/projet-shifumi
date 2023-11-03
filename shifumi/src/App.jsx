65aec42835e74b229aa6920ccba89edb3bc51304
rob1bouchet <rob1bouchet@gmail.com>
2023-11-03 14:49:31 +0100

    started auth

--- shifumi/src/App.jsx
+++ shifumi/src/App.jsx
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import { Stack } from '@mui/joy';
-import FormControl from '@mui/joy/FormControl';
-import Input from '@mui/joy/Input';
-import Button from '@mui/joy/Button';
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import Authentification from './components/Authentification';
 
 function App() {
-  return (
+
+  return(
     <>
-      <h1>ShiFuMi in React</h1>
-      <form>
-      <FormControl>
-        <Stack direction="column" spacing={1}>
-          <Input placeholder="username" required />
-          <Input placeholder="password" required />
-          <Stack direction="row" spacing={3} justifyContent="space-evenly">
-            <Button type="submit" onSubmit={handleLogin}>Login</Button>
-            <Button type="submit" onSubmit={handleRegister}>Register</Button>
-          </Stack>
-        </Stack>
-      </FormControl>
-      </form>
+    <h1>ShiFuMi in React</h1>
+    <Authentification/>
     </>
-  )
+  );
+ 
 }
 
-export default App
+export default App;
---
+++ shifumi/src/components/Authentification.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useState } from 'react';
+import { Stack } from '@mui/joy';
+import FormControl from '@mui/joy/FormControl';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+
+export default function Authentification() {
+
+const handleLogin = (userInfo) => {
+    console.log('login');
+    fetch('http://localhost:3002/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const handleRegister = (userInfo) => {
+    console.log('register');
+    fetch('http://localhost:3002/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const userInfo = {
+      username: event.target[0].value,
+      password: event.target[1].value,
+    };
+    if (event.nativeEvent.submitter.name === 'login') {
+      handleLogin(userInfo);
+    } else if (event.nativeEvent.submitter.name === 'register') {
+      handleRegister(userInfo);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Stack direction="column" spacing={1}>
+          <Input placeholder="username" required />
+          <Input placeholder="password" required />
+          <Stack direction="row" spacing={3} justifyContent="space-evenly">
+            <Button type="submit" name="login">
+              Login
+            </Button>
+            <Button type="submit" name="register">
+              Register
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
+    </>
+  );
+
+}
